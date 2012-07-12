7e7e18f4d64043a09578fd52f8d489751a709377
Thomas Holder <speleo3@users.sourceforge.net>
2012-07-12 11:28:49 +0200

    modelling.py: several new modelling functions
    - add_missing_atoms: mutate residues with missing atoms in place
    - peptide_rebuild: creates a new object based on sequence and
      coordinates from selection
    - peptide_rebuild_modeller: creates a new object with modeller
    - mutate: new mode=2

--- psico/modelling.py
+++ psico/modelling.py
@@ -34,6 +34,7 @@ ARGUMENTS
 
     mode = 0: select rotamer with best clash score {default}
     mode = 1: take chi angles from original residue
+    mode = 2: first rotamer
 
 EXAMPLE
 
@@ -89,7 +90,12 @@ EXAMPLE
             'VAL': ['C','CA','CB','CG2'],
         }
         atoms = [res + '/' + name for name in chi_atoms.get(old_resn, [])]
-        old_chi = [cmd.get_dihedral(*args) for args in zip(atoms, atoms[1:], atoms[2:], atoms[3:])]
+        old_chi = []
+        for args in zip(atoms, atoms[1:], atoms[2:], atoms[3:]):
+            try:
+                old_chi.append(cmd.get_dihedral(*args))
+            except:
+                break
 
     cmd.remove('%s and not name CA+C+N+O+OXT' % (res))
 
@@ -117,7 +123,7 @@ EXAMPLE
             print ' Best: Frame %d Score %.2f' % best_state
         return best_state
 
-    if cmd.count_states(mutagenesis.obj_name) < 2 or mode == 1:
+    if cmd.count_states(mutagenesis.obj_name) < 2 or mode > 0:
         best_state = (1, -1.0)
     else:
         best_state = get_best_state_bump()
@@ -194,7 +200,8 @@ USAGE
     elif model != org:
         sele = sele.replace('(%s)' % org, '(%s)' % model)
 
-    cmd.protect('not (%s)' % (sele))
+    cmd.protect()
+    cmd.deprotect(sele)
     if not backbone:
         cmd.protect('name CA+C+N+O+OXT')
 
@@ -219,14 +226,280 @@ USAGE
     cmd.sculpt_deactivate(model)
     cmd.deprotect()
 
+def add_missing_atoms(selection='all', cycles=200, quiet=1):
+    '''
+DESCRIPTION
+
+    Mutate those residues to themselves which have missing atoms
+
+SEE ALSO
+
+    stub2ala
+    '''
+    from collections import defaultdict
+    from chempy import fragments
+
+    cycles, quiet = int(cycles), int(quiet)
+
+    reference = {
+        'ALA': set(['CB']),
+        'ARG': set(['CB', 'CG', 'NE', 'CZ', 'NH1', 'NH2', 'CD']),
+        'ASN': set(['CB', 'CG', 'OD1', 'ND2']),
+        'ASP': set(['CB', 'CG', 'OD1', 'OD2']),
+        'CYS': set(['CB', 'SG']),
+        'GLN': set(['CB', 'CG', 'CD', 'NE2', 'OE1']),
+        'GLU': set(['CB', 'CG', 'OE2', 'CD', 'OE1']),
+        'GLY': set([]),
+        'HIS': set(['CE1', 'CB', 'CG', 'CD2', 'ND1', 'NE2']),
+        'ILE': set(['CB', 'CD1', 'CG1', 'CG2']),
+        'LEU': set(['CB', 'CG', 'CD1', 'CD2']),
+        'LYS': set(['CB', 'CG', 'NZ', 'CE', 'CD']),
+        'MET': set(['CB', 'CG', 'CE', 'SD']),
+        'PHE': set(['CE1', 'CB', 'CG', 'CZ', 'CD1', 'CD2', 'CE2']),
+        'PRO': set(['CB', 'CG', 'CD']),
+        'SER': set(['OG', 'CB']),
+        'THR': set(['CB', 'OG1', 'CG2']),
+        'TRP': set(['CZ2', 'CB', 'CG', 'CH2', 'CE3', 'CD1', 'CD2', 'CZ3', 'NE1', 'CE2']),
+        'TYR': set(['CE1', 'OH', 'CB', 'CG', 'CZ', 'CD1', 'CD2', 'CE2']),
+        'VAL': set(['CB', 'CG1', 'CG2']),
+    }
+
+    namedsele = cmd.get_unused_name('_')
+    cmd.select(namedsele, selection, 0)
+
+    namelists = defaultdict(list)
+    cmd.iterate('(%s) and polymer' % namedsele,
+            'namelists[model,segi,chain,resn,resi,resv].append(name)',
+            space=locals())
+
+    sele_dict = defaultdict(list)
+    tmp_name = cmd.get_unused_name('_')
+
+    for key, namelist in namelists.iteritems():
+        resn = key[3]
+        if resn not in reference:
+            if not quiet:
+                print ' Unknown residue:', resn
+            continue
+        if not reference[resn].issubset(namelist):
+            try:
+                frag = fragments.get(resn.lower())
+                for a in frag.atom:
+                    a.segi = key[1]
+                    a.chain = key[2]
+                    a.resi = key[4]
+                    a.resi_number = key[5]
+                cmd.load_model(frag, tmp_name, 1, zoom=0)
+
+                skey = '/%s/%s/%s/%s`%s' % key[:5]
+                cmd.remove(skey + ' and not name N+C+O+OXT+CA')
+                cmd.align(tmp_name, skey + ' and name N+C+CA', cycles=0)
+                cmd.remove(tmp_name + ' and (name N+C+O+CA or hydro)')
+                cmd.fuse('name CB and ' + tmp_name, 'name CA and ' + skey, move=0)
+                cmd.unpick()
+                cmd.delete(tmp_name)
+
+                sele_dict[key[0]].append(skey)
+
+                if not quiet:
+                    print ' Mutated ', skey
+            except:
+                print ' Mutating', skey, 'failed'
+
+    for model in sele_dict:
+        cmd.sort(model)
+        sculpt_relax(' '.join(sele_dict[model]), 0, 0, model, cycles)
+
+    cmd.delete(namedsele)
+
+def peptide_rebuild(name, selection='all', cycles=1000, state=1, quiet=1):
+    '''
+DESCRIPTION
+
+    Rebuild the peptide from selection. All atoms which are present in
+    selection will be kept fixed, while atoms missing in selection are
+    placed by sculpting.
+
+USAGE
+
+    peptide_rebuild name [, selection [, cycles [, state ]]]
+
+SEE ALSO
+
+    stub2ala, add_missing_atoms, peptide_rebuild_modeller
+    '''
+    from chempy import fragments, feedback, models
+
+    cycles, state, quiet = int(cycles), int(state), int(quiet)
+
+    # suppress feedback for model merging
+    feedback['actions'] = False
+
+    # work with named selection
+    namedsele = cmd.get_unused_name('_')
+    cmd.select(namedsele, selection, 0)
+
+    identifiers = []
+    cmd.iterate(namedsele + ' and polymer and guide and alt +A',
+            'identifiers.append([segi,chain,resi,resv,resn])', space=locals())
+
+    model = models.Indexed()
+    for (segi,chain,resi,resv,resn) in identifiers:
+        try:
+            fname = resn.lower() if resn != 'MSE' else 'met'
+            frag = fragments.get(fname)
+        except IOError:
+            print ' Warning: unknown residue:', resn
+            continue
+
+        for a in frag.atom:
+            a.segi = segi
+            a.chain = chain
+            a.resi = resi
+            a.resi_number = resv
+            a.resn = resn
+
+        model.merge(frag)
+
+    if not quiet:
+        print ' Loading model...'
+
+    cmd.load_model(model, name, 1, zoom=0)
+    if cmd.get_setting_boolean('auto_remove_hydrogens'):
+        cmd.remove(name + ' and hydro')
+
+    cmd.protect(name + ' in ' + namedsele)
+    cmd.sculpt_activate(name)
+    cmd.update(name, namedsele, 1, state)
+    cmd.delete(namedsele)
+
+    if not quiet:
+        print ' Sculpting...'
+
+    cmd.set('sculpt_field_mask', 0x003, name) # bonds and angles only
+    cmd.sculpt_iterate(name, 1, int(cycles / 4))
+
+    cmd.set('sculpt_field_mask', 0x09F, name) # local + torsions
+    cmd.sculpt_iterate(name, 1, int(cycles / 4))
+
+    cmd.set('sculpt_field_mask', 0x0FF, name) # ... + vdw
+    cmd.sculpt_iterate(name, 1, int(cycles / 2))
+
+    cmd.sculpt_deactivate(name)
+    cmd.deprotect(name)
+    cmd.unset('sculpt_field_mask', name)
+
+    if not quiet:
+        print ' Connecting peptide...'
+
+    pairs = cmd.find_pairs(name + ' and name C', name + ' and name N', 1, 1, 2.0)
+    for pair in pairs:
+        cmd.bond(*pair)
+    cmd.h_fix(name)
+
+    if not quiet:
+        print ' peptide_rebuild: done'
+
+def peptide_rebuild_modeller(name, selection='all', hetatm=0, sequence=None, quiet=1):
+    '''
+DESCRIPTION
+
+    Remodel the given selection using modeller. This is useful for example to
+    build incomplete sidechains. More complicated modelling tasks are not
+    the intention of this simple interface.
+
+    Side effects: Alters "type" property for MSE residues in selection
+    (workaround for bug #3512313).
+
+USAGE
+
+    peptide_rebuild_modeller name [, selection [, hetatm [, sequence ]]]
+
+ARGUMENTS
+
+    name = string: new object name
+
+    selection = string: atom selection
+
+    hetatm = 0/1: read and model HETATMs (ligands) {default: 0}
+
+    sequence = string: if provided, use this sequence instead of the
+    template sequence {default: None}
+    '''
+    try:
+        import modeller
+        from modeller.automodel import automodel
+    except ImportError:
+        print ' Error: failed to import "modeller"'
+        raise CmdException
+
+    import tempfile, shutil, os
+    from .editing import update_identifiers
+
+    hetatm, quiet = int(hetatm), int(quiet)
+
+    tempdir = tempfile.mkdtemp()
+    pdbfile = os.path.join(tempdir, 'template.pdb')
+    alnfile = os.path.join(tempdir, 'aln.pir')
+
+    cwd = os.getcwd()
+    os.chdir(tempdir)
+
+    if not quiet:
+        print ' Notice: PWD=%s' % (tempdir)
+
+    try:
+        modeller.log.none()
+        env = modeller.environ()
+        env.io.hetatm = hetatm
+
+        # prevent PyMOL to put TER records before MSE residues (bug #3512313)
+        cmd.alter('(%s) and polymer' % (selection), 'type="ATOM"')
+
+        cmd.save(pdbfile, selection)
+        mdl = modeller.model(env, file=pdbfile)
+
+        aln = modeller.alignment(env)
+        aln.append_model(mdl, align_codes='foo', atom_files=pdbfile)
+        if sequence:
+            aln.append_sequence(sequence)
+            aln[-1].code = 'bar'
+            aln.malign()
+        aln.write(alnfile)
+
+        a = automodel(env, alnfile=alnfile, sequence=aln[-1].code,
+                knowns=[s.code for s in aln if s.prottyp.startswith('structure')])
+        a.max_ca_ca_distance = 30.0
+        a.make()
+
+        cmd.load(a.outputs[0]['name'], name, quiet=quiet)
+    finally:
+        os.chdir(cwd)
+        shutil.rmtree(tempdir)
+
+    cmd.align(name, selection, cycles=0)
+    if not sequence:
+        update_identifiers(name, selection)
+
+    if not quiet:
+        print ' peptide_rebuild_modeller: done'
+
 cmd.extend('mutate', mutate)
 cmd.extend('mutate_all', mutate_all)
 cmd.extend('sculpt_relax', sculpt_relax)
+cmd.extend('add_missing_atoms', add_missing_atoms)
+cmd.extend('peptide_rebuild', peptide_rebuild)
+cmd.extend('peptide_rebuild_modeller', peptide_rebuild_modeller)
 
 cmd.auto_arg[0].update([
     ('mutate', cmd.auto_arg[0]['align']),
     ('mutate_all', cmd.auto_arg[0]['align']),
     ('sculpt_relax', cmd.auto_arg[0]['align']),
+    ('add_missing_atoms', cmd.auto_arg[0]['zoom']),
+])
+cmd.auto_arg[1].update([
+    ('peptide_rebuild', cmd.auto_arg[0]['zoom']),
+    ('peptide_rebuild_modeller', cmd.auto_arg[0]['zoom']),
 ])
 
 # vi: expandtab:smarttab
