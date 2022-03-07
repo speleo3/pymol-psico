3c7730882e879fe5d23e57dd808b9642a4413b0e
Thomas Holder <thomas@thomas-holder.de>
2022-03-07 20:46:29 +0100

    Fix consurf test with missing MMTF support

--- tests/test_conservation.py
+++ tests/test_conservation.py
@@ -8,7 +8,7 @@ DATA_PATH = Path(__file__).resolve().parent / 'data'
 
 def test_load_consurf():
     cmd.reinitialize()
-    cmd.load(DATA_PATH / "1ubq.mmtf.gz", "m1")
+    cmd.load(DATA_PATH / "1ubq.cif.gz", "m1")
     cmd.color("white")
     psico.conservation.load_consurf(
         str(DATA_PATH / "consurf_summary-5NVGA.txt"), "m1")
