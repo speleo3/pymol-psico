fd962b3c5f8218ec234a6a3a40506275c3e27a55
Thomas Holder <thomas.holder@rentschler.de>
2023-08-01 19:07:30 +0200

    Split test_mcsalign

--- tests/test_mcsalign.py
+++ tests/test_mcsalign.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.rdkit
-def test_mcsalign():
+def test_mcsalign__rdkit():
     cmd.reinitialize()
     cmd.fragment("his")
     cmd.fragment("trp")
@@ -13,6 +13,13 @@ def test_mcsalign():
     assert cmd.count_atoms("aln_r_0") == 20
     psico.mcsalign.mcsalign("his", "trp", object="aln_r_1", method="rdkit", exact=1)
     assert cmd.count_atoms("aln_r_1") == 18
+
+
+def test_mcsalign__indigo():
+    cmd.reinitialize()
+    cmd.fragment("his")
+    cmd.fragment("trp")
+    cmd.remove("hydro")
     psico.mcsalign.mcsalign("his", "trp", object="aln_i_0", method="indigo")
     assert cmd.count_atoms("aln_i_0") == 18
     psico.mcsalign.mcsalign("his", "trp", object="aln_i_1", method="indigo", exact=1)
