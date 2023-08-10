019f813d580abd2650e594ecab40f814a1e972cf
Thomas Holder <thomas.holder@rentschler.de>
2023-08-10 21:45:07 +0200

    Fix test_minimize_rdkit for rdkit<2022.09.1

--- tests/test_minimizing.py
+++ tests/test_minimizing.py
@@ -42,6 +42,7 @@ def test_minimize_ob():
 
 @pytest.mark.rdkit
 def test_minimize_rdkit():
+    import rdkit
     cmd.reinitialize()
     cmd.fab("ACD", "m1")
     cmd.copy("m2", "m1")
@@ -50,7 +51,8 @@ def test_minimize_rdkit():
     assert r == approx(0.7118, abs=1e-2)
     psico.minimizing.minimize_rdkit("m2", nsteps=100, ff="UFF")
     r = cmd.rms("m2", "m1")
-    assert r == approx(0.9508, abs=1e-2)
+    abs_UFF = 1e-1 if rdkit.__version__ < '2022.09.1' else 1e-2
+    assert r == approx(0.9508, abs=abs_UFF)
 
 
 @pytest.mark.openbabel
