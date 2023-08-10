0dbd22f622077ec23d46e1969f4dd2486e8cae4f
Thomas Holder <thomas.holder@rentschler.de>
2023-08-10 20:31:22 +0200

    v4.3

--- psico/__init__.py
+++ psico/__init__.py
@@ -11,7 +11,7 @@ PyMOL> psico.helping.write_html_ref('psico-commands.html')
 License: BSD-2-Clause
 '''
 
-__version__ = '4.2'
+__version__ = '4.3'
 
 from psico.versioning import make_version_int_tuple
 
